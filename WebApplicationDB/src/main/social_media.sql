6019e07a3b6839dd45001eb5ca2f70d346029070
vgr <neilalfonzcasas@gmail.com>
2025-03-01 13:25:30 +0800

    Update social_media.sql

--- WebApplicationDB/src/main/social_media.sql
+++ WebApplicationDB/src/main/social_media.sql
@@ -1,31 +1,41 @@
 CREATE DATABASE IF NOT EXISTS social_media;
 USE social_media;
+
+
 CREATE TABLE account (
     user_name VARCHAR(50) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
-    user_role ENUM('admin', 'user', 'super_admin') NOT NULL
+    user_role ENUM('admin', 'user') NOT NULL
 );
 
+
 CREATE TABLE posts (
-    post_id INT AUTO_INCREMENT PRIMARY KEY,
+
     user_name VARCHAR(50),
-    content VARCHAR(200) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    post1 VARCHAR(200),
+    post2 VARCHAR(200),
+    post3 VARCHAR(200),
+    post4 VARCHAR(200),
+    post5 VARCHAR(200),
+    PRIMARY KEY (user_name),
     FOREIGN KEY (user_name) REFERENCES account(user_name) ON DELETE CASCADE
 );
 
+
 CREATE TABLE follows (
     user_name VARCHAR(50),
-    followed_user_name VARCHAR(50),
-    PRIMARY KEY (user_name, followed_user_name),
+    follow1 VARCHAR(50),
+    follow2 VARCHAR(50),
+    follow3 VARCHAR(50),
+    PRIMARY KEY (user_name),
     FOREIGN KEY (user_name) REFERENCES account(user_name) ON DELETE CASCADE,
-    FOREIGN KEY (followed_user_name) REFERENCES account(user_name) ON DELETE CASCADE
+    FOREIGN KEY (follow1) REFERENCES account(user_name) ON DELETE SET NULL,
+    FOREIGN KEY (follow2) REFERENCES account(user_name) ON DELETE SET NULL,
+    FOREIGN KEY (follow3) REFERENCES account(user_name) ON DELETE SET NULL
 );
 
-CREATE TABLE admin_messages (
-    message_id INT AUTO_INCREMENT PRIMARY KEY,
-    sender VARCHAR(50),
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender) REFERENCES account(user_name) ON DELETE CASCADE
-);
+
+
+
+
+
